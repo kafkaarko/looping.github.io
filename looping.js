da3b385b91f24e4b0018855882a2002541fc8854
kafkaarko <kafkaarko@gmail.com>
2023-12-16 00:02:02 +0700

    ini js pertama

---
+++ looping.js
@@ -0,0 +1,25 @@
+for(let i = 1 ; i <= 10 ; i++){
+    document.write('<p>angka:'+i+'</p>')
+}
+
+for(let i = 10 ; i > 0 ; i--){
+    document.write('<p>angka:'+i+'</p>')
+}
+
+ i = 0
+while(i <= 10){
+    console.log('angka' + i)
+    i++
+}
+
+ a = 10
+while(a >= 0){
+    console.log('angka' + a)
+    a--
+}
+
+p = 2023
+do{
+    p--
+    console.log(p)
+}while(p > 1999)
