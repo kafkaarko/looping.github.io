7d197525129e0630e8fc117cf11c18751403f33d
kafkaarko <kafkaarko@gmail.com>
2024-01-02 09:47:50 +0700

    perubahan

--- looping.js
+++ looping.js
@@ -1,18 +1,18 @@
-for(let i = 1 ; i <= 10 ; i++){
+for(let i = 1 ; i <= 50 ; i++){
     document.write('<p>angka:'+i+'</p>')
 }
 
-for(let i = 10 ; i > 0 ; i--){
+for(let i = 50 ; i > 0 ; i--){
     document.write('<p>angka:'+i+'</p>')
 }
 
  i = 0
-while(i <= 10){
+while(i <= 50){
     console.log('angka' + i)
     i++
 }
 
- a = 10
+ a = 50
 while(a >= 0){
     console.log('angka' + a)
     a--
@@ -22,4 +22,4 @@ p = 2023
 do{
     p--
     console.log(p)
-}while(p > 1999)
+}while(p >= 1999)
